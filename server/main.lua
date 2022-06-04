07660f5116087d18bc2e628a841e2d7d48ff8b24
MaDHouSe79 <madhouse1979@gmail.com>
2022-06-05 01:44:28 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -15,8 +15,8 @@ end, 'admin')
 QBCore.Commands.Add(Config.JobCommands.police, "Give yourself police as a job", {}, true, function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob('police', 5)
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a police officer with rank 5 (Boss)')
+    Player.Functions.SetJob('police', Config.JobBossRank.police)
+    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a police officer with rank '..Config.JobBossRank.police..' (Boss)')
     TriggerEvent("police:server:UpdateCurrentCops")
     TriggerEvent("police:server:UpdateBlips")
 end, 'admin')
@@ -24,41 +24,38 @@ end, 'admin')
 QBCore.Commands.Add(Config.JobCommands.ambulance, "Give your self ambulance as a job", {}, true, function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob('mechanic', 5)
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as an ambulance with rank 5 (Boss)')
+    Player.Functions.SetJob('ambulance', Config.JobBossRank.ambulance)
+    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as an ambulance with rank '..Config.JobBossRank.ambulance..' (Boss)')
     TriggerEvent("police:server:UpdateBlips")
 end, 'admin')
 
 QBCore.Commands.Add(Config.JobCommands.mechanic, "Give your self mechanic as job", {}, true, function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob('mechanic', 4)
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a mechanic with rank 4 (Boss)')
+    Player.Functions.SetJob('mechanic', Config.JobBossRank.mechanic)
+    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a mechanic with rank '..Config.JobBossRank.mechanic..' (Boss)')
 end, 'admin')
 
 QBCore.Commands.Add(Config.JobCommands.cardealer, "Give your self cardealer as job", {}, true, function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob('cardealer', 4)
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a cardealer with rank 4 (Boss)')
+    Player.Functions.SetJob('cardealer', Config.JobBossRank.cardealer)
+    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a cardealer with rank '..Config.JobBossRank.cardealer..' (Boss)')
 end, 'admin')
 
 QBCore.Commands.Add(Config.JobCommands.realestate, "Give your self realestate as job", {}, true, function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob('realestate', 4)
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a realestate with rank 4 (Boss)')
+    Player.Functions.SetJob('realestate', Config.JobBossRank.realestate)
+    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a realestate with rank '..Config.JobBossRank.realestate..' (Boss)')
 end, 'admin')
 
 QBCore.Commands.Add(Config.JobCommands.unemployed, "Make yourself unemployed", {}, false, function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob('unemployed', 0)
+    Player.Functions.SetJob('unemployed', Config.JobBossRank.unemployed)
     TriggerClientEvent('QBCore:Notify', src, 'Congratulations you are now unemployed')
 end, 'admin')
-
-
-
 
 QBCore.Functions.CreateCallback('qb-adminjobs:server:haspermission', function(source, cb)
     if QBCore.Functions.HasPermission(source, 'admin') or QBCore.Functions.HasPermission(source, 'god') or IsPlayerAceAllowed(source, 'command') then
