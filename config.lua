c3f5179174c01c9a5c6b75d0abd20b22445f0c75
MaDHouSe79 <madhouse1979@gmail.com>
2022-06-05 01:40:24 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,24 @@
 Config                = {}
 
-
 Config.Commands       = {
     ['onduty']        = 'onduty',
     ['offduty']       = 'offduty',
 }
 
 Config.JobCommands    = {
-    ['unemployed']    = 'unemployed',
+    ['unemployed']    = 'nojob',
     ['police']        = 'poljob',
     ['ambulance']     = 'ambjob',
     ['mechanic']      = 'mechjob',
+    ['cardealer']     = 'carjob',
+    ['realestate']    = 'realjob',
+}
+
+Config.JobBossRank    = {
+    ['unemployed']    = 0,
+    ['police']        = 3,
+    ['ambulance']     = 3,
+    ['mechanic']      = 3
+    ['cardealer']     = 3,
+    ['realestate']    = 3,
 }
