5cb7a48ff2b07b17e7b63a7305bca275647c845a
MaDHouSe79 <madhouse1979@gmail.com>
2022-06-05 04:03:00 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,14 +1,13 @@
 --[[ ===================================================== ]]--
 --[[      QBCore Admin Job Changer Script by MaDHouSe      ]]--
 --[[ ===================================================== ]]--
-
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function SetJob(source, job, rank)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.SetJob(job, rank)
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations on your new job as a '..job..' with rank '..rank..' (Boss)')
+    TriggerClientEvent('QBCore:Notify', src, Lang:t('jobInfo.job_change', {job = job, rank = rank}))
     if job == 'police' then
         TriggerEvent("police:server:UpdateCurrentCops")
         TriggerEvent("police:server:UpdateBlips")
@@ -18,38 +17,14 @@ local function SetJob(source, job, rank)
     end
 end
 
-QBCore.Commands.Add('poljob', "Give yourself police as a job", {}, true, function(source)
-    SetJob(source, 'police', Config.JobBossRank.police)
-end, 'admin')
-
-QBCore.Commands.Add('ambjob', "Give your self ambulance as a job", {}, true, function(source)
-    SetJob(source, 'ambulance', Config.JobBossRank.ambulance)
-end, 'admin')
-
-QBCore.Commands.Add('mechjob', "Give your self mechanic as job", {}, true, function(source)
-    SetJob(source, 'mechanic', Config.JobBossRank.mechanic)
-end, 'admin')
-
-QBCore.Commands.Add('carjob', "Give your self cardealer as job", {}, true, function(source)
-    SetJob(source, 'cardealer', Config.JobBossRank.cardealer)
-end, 'admin')
-
-QBCore.Commands.Add('realjob', "Give your self realestate as job", {}, true, function(source)
-    SetJob(source, 'realestate', Config.JobBossRank.realestate)
-end, 'admin')
-
-QBCore.Commands.Add('nojob', "Make yourself unemployed", {}, false, function(source)
-    SetJob(source, 'unemployed', Config.JobBossRank.unemployed)
-end, 'admin')
-
-QBCore.Commands.Add('onduty', "Put yourself in service", {}, false, function(source)
-    TriggerClientEvent("qb-adminjobs:client:ToggleDuty", source)
-end, 'admin')
-
-QBCore.Commands.Add('offduty', "Take you out of service", {}, false, function(source)
-    TriggerClientEvent("qb-adminjobs:client:ToggleDuty", source)
-end, 'admin')
-
+QBCore.Commands.Add('poljob', Lang:t('jobInfo.police'), {}, true, function(source) SetJob(source, 'police', Config.JobBossRank.police) end, 'admin')
+QBCore.Commands.Add('ambjob', Lang:t('jobInfo.ambulance'), {}, true, function(source) SetJob(source, 'ambulance', Config.JobBossRank.ambulance) end, 'admin')
+QBCore.Commands.Add('mechjob', Lang:t('jobInfo.mechanic'), {}, true, function(source) SetJob(source, 'mechanic', Config.JobBossRank.mechanic) end, 'admin')
+QBCore.Commands.Add('carjob', Lang:t('jobInfo.cardealer'), {}, true, function(source) SetJob(source, 'cardealer', Config.JobBossRank.cardealer) end, 'admin')
+QBCore.Commands.Add('realjob', Lang:t('jobInfo.realestate'), {}, true, function(source) SetJob(source, 'realestate', Config.JobBossRank.realestate) end, 'admin')
+QBCore.Commands.Add('nojob', Lang:t('jobInfo.unemployed'), {}, false, function(source) SetJob(source, 'unemployed', Config.JobBossRank.unemployed) end, 'admin')
+QBCore.Commands.Add('onduty', Lang:t('jobInfo.onduty'), {}, false, function(source) TriggerClientEvent("qb-adminjobs:client:ToggleDuty", source) end, 'admin')
+QBCore.Commands.Add('offduty', Lang:t('jobInfo.offduty'), {}, false, function(source) TriggerClientEvent("qb-adminjobs:client:ToggleDuty", source) end, 'admin')
 QBCore.Functions.CreateCallback('qb-adminjobs:server:haspermission', function(source, cb)
     if QBCore.Functions.HasPermission(source, 'admin') or QBCore.Functions.HasPermission(source, 'god') or IsPlayerAceAllowed(source, 'command') then
         cb(true)
