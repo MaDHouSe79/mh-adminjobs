7538816a16e4de79d7ed3f52363686a11d8dd953
MaDHouSe <madhouse1979@gmail.com>
2023-12-26 14:15:23 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,27 @@
+--[[ ===================================================== ]]--
+--[[        MH Admin Job Changer Script by MaDHouSe        ]]--
+--[[ ===================================================== ]]--
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'MaDHouSe'
-description 'QB Adminjob Changer'
+author 'MaDHouSe79'
+description 'MH Adminjob Changer'
 version '1.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
+    '@qb-core/shared/jobs.lua',
     'locales/en.lua', -- change en to your language
-    'config.lua',
 }
 
 client_scripts {
     'client/main.lua',
 }
+
 server_scripts {
+    '@oxmysql/lib/MySQL.lua',
     'server/main.lua',
+    'server/update.lua',
 }
 
 dependencies {
