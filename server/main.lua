748b6d45059fb55b539d4980e11986d9462549ff
MaDHouSe <madhouse1979@gmail.com>
2023-12-26 14:14:53 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,59 +1,48 @@
---[[ ===================================================== ]]--
---[[      QBCore Admin Job Changer Script by MaDHouSe      ]]--
---[[ ===================================================== ]]--
+--[[ ===================================================== ]] --
+--[[        MH Admin Job Changer Script by MaDHouSe        ]] --
+--[[ ===================================================== ]] --
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local function SetJob(source, job, rank)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetJob(job, rank)
-    TriggerClientEvent('QBCore:Notify', src, Lang:t('jobInfo.job_change', {job = job, rank = rank}))
-    if job == 'police' then
-        TriggerEvent("police:server:UpdateCurrentCops")
-        TriggerEvent("police:server:UpdateBlips")
-    end     
-    if job == 'ambulance' then
-        TriggerEvent("police:server:UpdateBlips")
+local function isAdmin(source)
+    if IsPlayerAceAllowed(source, 'admin') then
+        return true
     end
-    Player.Functions.SetJobDuty(true)
+    if IsPlayerAceAllowed(source, 'command') then
+        return true
+    end
+    return false
 end
 
-QBCore.Commands.Add('nojob', Lang:t('jobInfo.unemployed'), {}, false, function(source) 
-    SetJob(source, 'unemployed', Config.JobBossRank.unemployed) 
-end, 'admin')
-
-QBCore.Commands.Add('poljob', Lang:t('jobInfo.police'), {}, true, function(source) 
-    SetJob(source, 'police', Config.JobBossRank.police) 
-end, 'admin')
-
-QBCore.Commands.Add('ambjob', Lang:t('jobInfo.ambulance'), {}, true, function(source) 
-    SetJob(source, 'ambulance', Config.JobBossRank.ambulance) 
-end, 'admin')
-
-QBCore.Commands.Add('mechjob', Lang:t('jobInfo.mechanic'), {}, true, function(source) 
-    SetJob(source, 'mechanic', Config.JobBossRank.mechanic) 
-end, 'admin')
-
-QBCore.Commands.Add('carjob', Lang:t('jobInfo.cardealer'), {}, true, function(source) 
-    SetJob(source, 'cardealer', Config.JobBossRank.cardealer) 
-end, 'admin')
-
-QBCore.Commands.Add('realjob', Lang:t('jobInfo.realestate'), {}, true, function(source) 
-    SetJob(source, 'realestate', Config.JobBossRank.realestate) 
-end, 'admin')
-
-QBCore.Commands.Add('onduty', Lang:t('jobInfo.onduty'), {}, false, function(source) 
-    TriggerClientEvent("qb-adminjobs:client:ToggleDuty", source) 
-end, 'admin')
-
-QBCore.Commands.Add('offduty', Lang:t('jobInfo.offduty'), {}, false, function(source) 
-    TriggerClientEvent("qb-adminjobs:client:ToggleDuty", source) 
-end, 'admin')
-
-QBCore.Functions.CreateCallback('qb-adminjobs:server:haspermission', function(source, cb)
-    if QBCore.Functions.HasPermission(source, 'admin') or QBCore.Functions.HasPermission(source, 'god') or IsPlayerAceAllowed(source, 'command') then
-        cb(true)
-    else
-        cb(false)
+local function SetJob(source, job, rank)
+    if isAdmin(source) then
+        local Player = QBCore.Functions.GetPlayer(source)
+        Player.Functions.SetJob(job, rank)
+        TriggerClientEvent('QBCore:Notify', source, Lang:t('jobInfo.job_change', {
+            job = job,
+            rank = rank
+        }))
+        if job == 'police' or job == 'ambulance' then
+            if job == 'police' then
+                TriggerEvent("police:server:UpdateCurrentCops")
+            end
+            TriggerEvent("police:server:UpdateBlips")
+        end
+        TriggerClientEvent("mh-adminjobs:client:ToggleDuty", source)
     end
+end
+
+RegisterNetEvent('mh-adminjobs:server:changeJob', function(job, rank)
+    local src = source
+    if isAdmin(src) then
+        SetJob(src, job, rank)
+    end
+end)
+
+QBCore.Functions.CreateCallback('mh-adminjobs:server:isAdmin', function(source, cb)
+    local src = source
+    local isAllowed = false
+    if isAdmin(source) then
+        isAllowed = true
+    end
+    cb(isAllowed)
 end)
