a7dfcccf57c6369e359e59722f824b512b807061
MaDHouSe <madhouse1979@gmail.com>
2023-12-26 14:17:58 +0100

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -1,14 +1,8 @@
+--[[ ===================================================== ]]--
+--[[        MH Admin Job Changer Script by MaDHouSe        ]]--
+--[[ ===================================================== ]]--
 local Translations = {
-
     jobInfo = {
-        ["police"]     = "Geef jezelf politie als een baan", 
-        ["ambulance"]  = "Geef jezelf ambulance als een baan", 
-        ["mechanic"]   = "Geef jezelf mechanic als een baan", 
-        ["cardealer"]  = "Geef jezelf cardealer als een baan", 
-        ["realestate"] = "Geef jezelf realestate als een baan", 
-        ["unemployed"] = "Maak jezelf koos werkloos", 
-        ["onduty"]     = "Ga in dienst", 
-        ["offduty"]    = "Ga uit dienst", 
         ["job_change"] = "Gefeliciteerd met je nieuwe baan als %{job} met de rang %{rank} (Baas)", 
     },
 }
