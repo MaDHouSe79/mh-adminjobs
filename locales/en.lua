155a44fc54ce721b990dbe74c24b656cea43c88c
MaDHouSe <madhouse1979@gmail.com>
2023-12-26 14:17:38 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,14 +1,8 @@
+--[[ ===================================================== ]]--
+--[[        MH Admin Job Changer Script by MaDHouSe        ]]--
+--[[ ===================================================== ]]--
 local Translations = {
-
     jobInfo = {
-        ["police"]     = "Give yourself police as a job", 
-        ["ambulance"]  = "Give your self ambulance as a job", 
-        ["mechanic"]   = "Give your self mechanic as job", 
-        ["cardealer"]  = "Give your self cardealer as job", 
-        ["realestate"] = "Give your self realestate as job", 
-        ["unemployed"] = "Make yourself unemployed", 
-        ["onduty"]     = "Put yourself in service", 
-        ["offduty"]    = "Take you out of service", 
         ["job_change"] = "Congratulations on your new job as a %{job} with rank %{rank} (Boss)", 
     },
 }
