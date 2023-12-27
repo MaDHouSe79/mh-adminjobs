0192c6c606789a81bb7ef0fecf6754e9d7e2fd4d
MaDHouSe <madhouse1979@gmail.com>
2023-12-27 09:30:20 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,12 +4,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function isAdmin(source)
-    if IsPlayerAceAllowed(source, 'admin') then
-        return true
-    end
-    if IsPlayerAceAllowed(source, 'command') then
-        return true
-    end
+    if IsPlayerAceAllowed(source, 'admin') then return true end
+    if IsPlayerAceAllowed(source, 'command') then return true end
     return false
 end
 
@@ -17,14 +13,9 @@ local function SetJob(source, job, rank)
     if isAdmin(source) then
         local Player = QBCore.Functions.GetPlayer(source)
         Player.Functions.SetJob(job, rank)
-        TriggerClientEvent('QBCore:Notify', source, Lang:t('jobInfo.job_change', {
-            job = job,
-            rank = rank
-        }))
+        TriggerClientEvent('QBCore:Notify', source, Lang:t('jobInfo.job_change', {job = job, rank = rank}))
         if job == 'police' or job == 'ambulance' then
-            if job == 'police' then
-                TriggerEvent("police:server:UpdateCurrentCops")
-            end
+            if job == 'police' then TriggerEvent("police:server:UpdateCurrentCops") end
             TriggerEvent("police:server:UpdateBlips")
         end
         TriggerClientEvent("mh-adminjobs:client:ToggleDuty", source)
@@ -33,16 +24,12 @@ end
 
 RegisterNetEvent('mh-adminjobs:server:changeJob', function(job, rank)
     local src = source
-    if isAdmin(src) then
-        SetJob(src, job, rank)
-    end
+    if isAdmin(src) then SetJob(src, job, rank) end
 end)
 
 QBCore.Functions.CreateCallback('mh-adminjobs:server:isAdmin', function(source, cb)
     local src = source
     local isAllowed = false
-    if isAdmin(source) then
-        isAllowed = true
-    end
+    if isAdmin(source) then isAllowed = true end
     cb(isAllowed)
 end)
