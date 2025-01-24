1e3b295ce7f6cedc60e0150bb9d7f1fe9688f1e8
MaDHouSe <madhouse1979@gmail.com>
2025-01-24 07:34:19 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,9 +6,10 @@ games { 'gta5' }
 repository 'https://github.com/MaDHouSe79/mh-adminjobs'
 author 'MaDHouSe79'
 description 'MH Adminjob - In game job changer for admins.'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
+    '@ox_lib/init.lua',
     '@qb-core/shared/locale.lua',
     '@qb-core/shared/jobs.lua',
     'locales/en.lua', -- change en to your language
