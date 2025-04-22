2f51be2b1d042ccae8921c9c1d8dbe9902e276cd
MaDHouSe <madhouse1979@gmail.com>
2025-04-22 09:09:22 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,33 +3,39 @@
 --[[ ===================================================== ]] --
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local function isAdmin(source)
-    if IsPlayerAceAllowed(source, 'admin') then return true end
-    if IsPlayerAceAllowed(source, 'command') then return true end
+local function isAdmin(src)
+    if IsPlayerAceAllowed(src, 'admin') or IsPlayerAceAllowed(src, 'command') then return true end
     return false
 end
 
-local function SetJob(source, job, rank)
-    if isAdmin(source) then
-        local Player = QBCore.Functions.GetPlayer(source)
-        Player.Functions.SetJob(job, rank)
-        TriggerClientEvent('QBCore:Notify', source, Lang:t('jobInfo.job_change', {job = job, rank = rank}))
-        if job == 'police' or job == 'ambulance' then
-            if job == 'police' then TriggerEvent("police:server:UpdateCurrentCops") end
-            TriggerEvent("police:server:UpdateBlips")
+local function SetJob(src, data)
+    if isAdmin(src) then
+        local Player = QBCore.Functions.GetPlayer(src)
+        if Player then
+            Player.Functions.SetJob(data.job, data.rank)
+            if data.job == 'police' or data.job == 'ambulance' then
+                if data.job == 'police' then TriggerEvent("police:server:UpdateCurrentCops") end
+                TriggerEvent("police:server:UpdateBlips")
+            end
+            TriggerClientEvent("mh-adminjobs:client:ToggleDuty", src)
+            return true
+        else
+            return false
         end
-        TriggerClientEvent("mh-adminjobs:client:ToggleDuty", source)
+    else
+        return false
     end
+    return false
 end
 
-RegisterNetEvent('mh-adminjobs:server:changeJob', function(job, rank)
-    local src = source
-    if isAdmin(src) then SetJob(src, job, rank) end
+QBCore.Functions.CreateCallback('mh-adminjobs:server:isAdmin', function(src, cb)
+    cb(isAdmin(src))
 end)
 
-QBCore.Functions.CreateCallback('mh-adminjobs:server:isAdmin', function(source, cb)
-    local src = source
-    local isAllowed = false
-    if isAdmin(source) then isAllowed = true end
-    cb(isAllowed)
+QBCore.Functions.CreateCallback('mh-adminjobs:server:changeJob', function(src, cb, data)
+    if isAdmin(src) then
+        cb(SetJob(src, data))
+        return
+    end
+    cb(false)
 end)
